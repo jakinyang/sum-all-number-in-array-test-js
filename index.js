cd22e1d3dc6804b573aedd55958d2585ed2f143f
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-22 13:04:20 -0700

    Solves for sumAllNuminArray

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
+    let tempSum = 0;
+    for(let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        tempSum += sumItems(array[i]);
+      } else {
+      tempSum += array[i];
+      }
+    }
+    return Number(tempSum);
+  }
+const test = [[1, 2, [[3], 4]], 5, []];
+console.log(sumItems(test));
 
 module.exports = sumItems;
